7b989ee6307c9d84bed95355cf488354517a09ca
Kelly <kellyrose.evergreen@gmail.com>
2017-08-28 22:52:40 +0900

    03 First HTTP Call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
+import {User} from "./shared/models/user";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  users: User[];
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+    //grab users
+    this.http.get('https://reqres.in/api/users')
+      .subscribe(data => {
+        console.log(data);
+        console.log(data.json());
+        this.users = data.json().data;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserCreateComponent } from './users/user-create/user-create.component';
+import { UsersComponent } from './users/users.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserSingleComponent } from './users/user-single/user-single.component';
+import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserCreateComponent,
+    UsersComponent,
+    UserEditComponent,
+    UserListComponent,
+    UserSingleComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
